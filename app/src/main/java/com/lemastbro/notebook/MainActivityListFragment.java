2abb082a693127a7cc08f2847e895e47718446bd
LeMastBro <lemastbro@gmail.com>
2016-08-28 15:24:49 -0700

    Vid 42, added ArrayAdapter -- Didn't work

--- app/src/main/java/com/lemastbro/notebook/MainActivityListFragment.java
+++ app/src/main/java/com/lemastbro/notebook/MainActivityListFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -18,15 +19,17 @@ public class MainActivityListFragment extends ListFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+    String[] values = new String[]{ "Android", "iPhone", "WindowsMobile",
+            "Blackberry", "WebOS", "Ubuntu", "Windows7", "Mac OS X",
+            "Linux", "OS/2" };
+
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+            android.R.simple_list_item_1, values );
+
+    setListAdapter(adapter);
+
     }
-
-
-        String values = new String[]("Android", "iPhone", "WindowsMobile", "Blackberry", "WebOS", "Ubuntu", "Windows7", "Mac OS X", "Linux", "OS/2");
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_1),
-
-
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
